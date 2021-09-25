fb097d4877a6e14702681a7779f92c5c3a9df465
MCGiako <giacometto.r.h2@gmail.com>
2021-09-25 18:16:06 +0000

    Traffic-Light-With-React terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,35 @@
-import React from "react";
-
+import React, { useState } from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [light, setLight] = useState({
+		light1: false,
+		light2: false,
+		light3: false
+	});
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1>Semáforo</h1>
+			<br />
+			<div id="div1">
+				<div
+					className={light.light1 ? "light1On" : "light1"}
+					onClick={() =>
+						setLight({ light1: true, luz2: false, luz3: false })
+					}></div>
+				<div
+					className={light.light2 ? "light2On" : "light2"}
+					onClick={() =>
+						setLight({ light1: false, light2: true, light3: false })
+					}></div>
+				<div
+					className={light.light3 ? "light3On" : "light3"}
+					onClick={() =>
+						setLight({ light1: false, light2: false, light3: true })
+					}></div>
+			</div>
 		</div>
 	);
 };
